8a573eff2c908825094819ca17a080c57071ce10
Daniele Della Cioppa <daniele.cioppa@akhter.co.uk>
2022-07-07 11:55:07 +0100

    java_threads

--- portfolio/Threads/src/com/company/Main.java
+++ portfolio/Threads/src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
-public class Main {
+public class Main implements Runnable
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+    System.out.println("ciao");
+//    HelloRunnable helloRunnable=new HelloRunnable();
+//    helloRunnable.run();
+      (new Thread(new HelloRunnable())).start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello from a thread!");
     }
 }
