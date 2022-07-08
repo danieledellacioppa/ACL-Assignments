dccd8b78a6ad21d108ccd7cb46cc8d8cbbc7d293
Daniele Della Cioppa <daniele.cioppa@akhter.co.uk>
2022-07-08 17:16:32 +0100

    thread

--- portfolio/Threads/src/com/company/Main.java
+++ portfolio/Threads/src/com/company/Main.java
@@ -5,16 +5,20 @@ public class Main
 
     public static void main(String[] args)
     {
+        HelloThread helloThread = new HelloThread();
+        System.out.println("helloThread.run();");
+        helloThread.run();
+        System.out.println("main() after helloThread launched"); //why is this message waiting for helloThread to finish?
+
+        //if it doesn't release the output nobody else can use it. that's a guess
+
       System.out.println("I am a simple Main class with simple main() launching threads");
 
       System.out.println("(new Thread(new HelloRunnable())).start();");
       (new Thread(new HelloRunnable())).start();
 
       System.out.println("HelloThread helloThread = new HelloThread();");
-      HelloThread helloThread = new HelloThread();
-      System.out.println("helloThread.run();");
-      helloThread.run();
-      System.out.println("main() after helloThread launched"); //why is this message waiting for helloThread to finish?
+
       System.out.println("main() about to finish");
     }
 
