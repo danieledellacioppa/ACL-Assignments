c3360445fa9e6fc0683a17d958e0a5ba67f5080c
Daniele Della Cioppa <daniele.cioppa@akhter.co.uk>
2022-07-12 17:30:41 +0100

    error_chasing

--- portfolio/Threads/src/com/company/HelloRunnable.java
+++ portfolio/Threads/src/com/company/HelloRunnable.java
@@ -3,6 +3,13 @@ package com.company;
 public class HelloRunnable implements Runnable {
 
     public void run() {
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Hello from HelloRunnable!");
     }
 
